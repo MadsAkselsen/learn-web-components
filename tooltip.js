80f77abcf4646c74cae7fce7c073351c15538b3e
Mads Akselsen <akselsenmads@gmail.com>
2021-01-23 16:31:47 +0100

    added shadowDOM

--- tooltip.js
+++ tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = 'Missing text on text attribute';
+    this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
@@ -16,23 +17,23 @@ class Tooltip extends HTMLElement {
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
-    this._tooltipContainer.style.zIndex = '10';
     this.style.position = 'relative';
+    this.shadowRoot.appendChild(tooltipIcon);
   }
 
   _showTooltip() {
     // adding underscore to indicate that this method is not be called
     this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.style.zIndex = '10';
     this._tooltipContainer.textContent = this._tooltipText;
     this._tooltipContainer.style.backgroundColor = 'black';
     this._tooltipContainer.style.color = 'white';
     this._tooltipContainer.style.position = 'absolute';
-    this.appendChild(this._tooltipContainer);
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
