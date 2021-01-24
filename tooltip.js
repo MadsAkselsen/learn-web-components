224cfb84f82df3905c6a575442c70d4da1b52651
Mads Akselsen <akselsenmads@gmail.com>
2021-01-24 18:23:36 +0100

    added component disconnect cleanup

--- tooltip.js
+++ tooltip.js
@@ -2,23 +2,31 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipIcon;
     this._tooltipText = 'Missing text on text attribute';
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
     <style>
       div {
+        font-weight: normal;
         background-color: black;
         color: white;
         position: absolute;
+        top: 1.5rem;
+        left: 0.75rem;
         z-index:10;
+        padding: 0.15rem;
+        border-radius: 3px;
+        box-shadow: 1px 1px 6px rgba(0,0,0,0.26)
       }
 
       :host(.important) {
-        background: #777;
+        background: var(--color-primary, #ccc);
+        padding: 0.15rem;
       }
 
       :host-context(p) {
-        font-size: 2rem;
+        
         font-weight: bold;
       }
  
@@ -46,11 +54,34 @@ class Tooltip extends HTMLElement {
     }
 
     // add new html elements and event listeners
-    const tooltipIcon = this.shadowRoot.querySelector('span');
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon.addEventListener(
+      'mouseenter',
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      'mouseleave',
+      this._hideTooltip.bind(this)
+    );
     this.style.position = 'relative';
-    this.shadowRoot.appendChild(tooltipIcon);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text'];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+    this._tooltipIcon.removeEventListener('mouseleave', this._showTooltip);
   }
 
   _showTooltip() {
